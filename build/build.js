81fc4e2fbffa8404efab93997ca7ffdc28f95e0c
Forbes Lindesay <fpfl2@cam.ac.uk>
2012-11-21 15:14:23 +0000

    Don't wrap server in an extra closure

--- build/build.js
+++ build/build.js
@@ -1697,10 +1697,6 @@ require.register("rooms/markdown/converter.js", function(module, exports, requir
 var Markdown = require('../libraries/pagedown.js');
 
 var converter = module.exports = Markdown.getSanitizingConverter();
-//Remove http from links display text
-converter.hooks.chain("plainLinkText", function (url) {
-    return url.replace(/^https?:\/\//, "");
-});
 //suport using #bang urls
 converter.hooks.chain("preConversion", function (text) {
     return text.replace(/\(#(.*)\)/g, "(http://hashurl.com/$1)")
@@ -5560,78 +5556,74 @@ template.templates = templates;
 module.exports = template;
 });
 require.register("rooms/server.js", function(module, exports, require){
-module.exports = (function () {
-    "use strict";
-
-    var prefix = "/rooms/";
-    function put(spec, isNew) {
-        var that = Object.create(spec);
-        that.method = "PUT";
-        if (isNew !== undefined) {
-            that.isNew = isNew;
-        }
-        return that;
+var prefix = "/rooms/";
+function put(spec, isNew) {
+    var that = Object.create(spec);
+    that.method = "PUT";
+    if (isNew !== undefined) {
+        that.isNew = isNew;
     }
-    function del(spec) {
-        var that = Object.create(spec);
-        that.method = "DELETE";
-        return that;
-    }
-    var markdownCache = {};
-    return {
-        prefix: prefix,
-        markdown: {
-            read: function (item, callback) {
-                var id = item.id;
-                if (markdownCache[id]) {
-                    setTimeout(function () { callback(markdownCache[id]); }, 0); //never call callback imediately
-                } else {
-                    $.getJSON(prefix + 'data/markdown', { id: id }, function (result) {
-                        function done(result) {
-                            markdownCache[id] = result;
-                            callback(result);
-                        }
-                        if (result === null) {
-                            var u = "===============================================";
-                            done(item.name + "\n" + ((item.name && u.substr(0, item.name.length)) || u.substr(0, id.length)) +
-                                        "\n\nThere is no description for this " + item.type + ", why not consider adding one?");
-                        } else {
-                            done(result);
-                        }
-                    });
-                }
-            },
-            update: function (id, content, callback) {
-                markdownCache[id] = content;
-                $.post(prefix + "data/markdown", put({ id: id, content: content }), callback);
+    return that;
+}
+function del(spec) {
+    var that = Object.create(spec);
+    that.method = "DELETE";
+    return that;
+}
+var markdownCache = {};
+module.exports = {
+    prefix: prefix,
+    markdown: {
+        read: function (item, callback) {
+            var id = item.id;
+            if (markdownCache[id]) {
+                setTimeout(function () { callback(markdownCache[id]); }, 0); //never call callback imediately
+            } else {
+                $.getJSON(prefix + 'data/markdown', { id: id }, function (result) {
+                    function done(result) {
+                        markdownCache[id] = result;
+                        callback(result);
+                    }
+                    if (result === null) {
+                        var u = "===============================================";
+                        done(item.name + "\n" + ((item.name && u.substr(0, item.name.length)) || u.substr(0, id.length)) +
+                                    "\n\nThere is no description for this " + item.type + ", why not consider adding one?");
+                    } else {
+                        done(result);
+                    }
+                });
             }
         },
-        navigation: {
-            create: function (spec, callback) {
-                $.post(prefix + "data/navigation", put(spec, true), callback);
-            },
-            read: function (callback) {
-                $.getJSON(prefix + 'data/navigation', callback);
-            },
-            update: function (id, name, value, success) {
-                $.post(prefix + 'data/navigation', put({ id: id, name: name, value: value }, false), success);
-            },
-            del: function (id, success) {
-                $.post(prefix + 'data/navigation', del({ id: id }), success);
-            }
-        },
-        allocation: {
-            update: function (roomid, year, crsid, callback) {
-                $.post(prefix + 'data/allocations', put({ roomid: roomid, year: year, crsid: crsid }), callback);
-            }
-        },
-        choosingOrder: {
-            read: function (callback) {
-                $.getJSON(prefix + 'data/choosingorder', callback);
-            }
+        update: function (id, content, callback) {
+            markdownCache[id] = content;
+            $.post(prefix + "data/markdown", put({ id: id, content: content }), callback);
         }
-    };
-})();
+    },
+    navigation: {
+        create: function (spec, callback) {
+            $.post(prefix + "data/navigation", put(spec, true), callback);
+        },
+        read: function (callback) {
+            $.getJSON(prefix + 'data/navigation', callback);
+        },
+        update: function (id, name, value, success) {
+            $.post(prefix + 'data/navigation', put({ id: id, name: name, value: value }, false), success);
+        },
+        del: function (id, success) {
+            $.post(prefix + 'data/navigation', del({ id: id }), success);
+        }
+    },
+    allocation: {
+        update: function (roomid, year, crsid, callback) {
+            $.post(prefix + 'data/allocations', put({ roomid: roomid, year: year, crsid: crsid }), callback);
+        }
+    },
+    choosingOrder: {
+        read: function (callback) {
+            $.getJSON(prefix + 'data/choosingorder', callback);
+        }
+    }
+};
 });
 require.register("rooms/stream.js", function(module, exports, require){
 var server = require('./server');
