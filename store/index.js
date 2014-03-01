41339ab3d9de9b2c349d42918cade4ba71c369b1
Forbes Lindesay <forbes@lindesay.co.uk>
2014-03-01 16:19:55 +0000

    Use mongo for all of the backend

--- store/index.js
+++ store/index.js
@@ -1,135 +1,64 @@
-var join = require('path').join;
-var fs = require('fs');
-var s3Client = require('./s3-client')();
-var mongoClient = require('./mongo-client')();
+'use strict';
 
-var isReady = false;
-var waitingForReady = [];
-var readyWaitingFor = 0;
-exports.ready = function (fn) {
-  if (isReady) fn();
-  else waitingForReady.push(fn);
-};
-function setReady() {
-  console.log('==ready==');
-  if (isReady) return;
-  isReady = true;
-  for (var i = 0; i < waitingForReady.length; i++) {
-    waitingForReady[i]();
-  }
-  waitingForReady = null;
-  exports.ready = function (fn) { fn() };
-};
-function waitFor() {
-  console.log('==start task==');
-  readyWaitingFor++;
-  return function () {
-    console.log('==end task==');
-    if (0 === --readyWaitingFor) setReady();
-  };
-}
+var mongod = require('mongod');
+var db = mongod(process.env.ROOMS_MONGO, [
+  'markdown',
+  'allocations',
+  'navigation'
+]);
 
-require('./download-from-s3')(waitFor());
 
 var markdown = exports.markdown = {};
 
-markdown.read = function (key, callback) {
-
-};
-markdown.path = function (key, callback) {
-  var path = join(__dirname, '..', 'data', 'markdown', (key || 'home') + '.md');
-  fs.stat(path, function (err) {
-    if (err) callback(err);
-    else callback(null, path);
-  })
+markdown.read = function (key) {
+  return db.markdown.findOne({_id: key}).then(function (res) {
+    return (res && res.body) || '';
+  });
 };
 markdown.update = function (key, content, callback) {
-  var localPath = join(__dirname, '..', 'data', 'markdown', (key || 'home') + '.md');
-  var serverPath = 'markdown/' + (key || 'home') + '.md';
-
-  fs.writeFile(localPath, content, function (err) {
-    if (err) return callback(err);
-    s3Client.putFile(localPath, serverPath, uploaded);
-  });
-  function uploaded(err, result) {
-    if (err) return callback(err);
-    if (result && result.statusCode && result.statusCode != 200)
-      return callback(new Error('server response code ' + result.statusCode));
-    callback();
-  }
+  return db.markdown.update({_id: key}, {_id: id, body: content}, {upsert: true});
 };
 
 var allocations = exports.allocations = {};
 
 allocations.get = function (year, callback) {
-  mongoClient.allocations.findOne({year: +year}, function (err, res) {
-    if (err) return callback(err);
-    else callback(null, (res && res.allocations) || {});
+  return db.allocations.findOne({year: +year}).then(function (res) {
+    return (res && res.allocations) || {};
   });
 };
 
-//{ year, roomid, crsid: name }
-allocations.list = function (callback) {
+// return a list of the form { year, roomid, crsid: name }
+// for all the time span the client may be interested in
+allocations.list = function () {
   var current = (new Date()).getFullYear();
   var last = current - 1;
   var next = current + 1;
-  var allocationsList = [];
-  var pending = 3;
-  getYear(last);
-  getYear(current);
-  getYear(next);
-  function getYear(year) {
-    allocations.get(year, function (err, res) {
-      if (err) return callback(err);
-      Object.keys(res)
-        .forEach(function (room) {
-          allocationsList.push({
-            year: year,
-            roomid: room,
-            crsid: res[room]
-          });
-        });
-      if (--pending === 0) return callback(null, allocationsList);
-    })
-  }
-};
-
-allocations.list.current = [];
-var gotAlloctions = waitFor();
-allocations.list(function (err, res) {
-  if (err) throw err;
-  allocations.list.current = res;
-  gotAlloctions();
-});
-
-allocations.set = function (year, room, name, callback) {
-  var update = {};
-  update['allocations.' + room] = name;
-  mongoClient.allocations.update({year: +year}, {$set: update}, 
-    {upsert: true}, function (err) {
-      if (err) return callback(err);
-
-      //update memory cache
-      var updated = false;
-      allocations.list.current.forEach(function (allocation) {
-        if (allocation.year === year && allocation.roomid === room) {
-          allocation.crsid = name;
-          updated = true;
-        }
-      });
-      if (!updated) {
-        allocations.list.current.push({
+  return Promise.all([
+    allocations.get(last),
+    allocations.get(current),
+    allocations.get(next)
+  ]).then(function (allocations) {
+    return allocations.map(function (obj) {
+      return Object.keys(obj).map(function (room) {
+        return {
           year: year,
           roomid: room,
-          crsid: name
-        });
-      }
+          crsid: res[room]
+        };
+      });
+    }).reduce(function (a, b) {
+      return a.concat(b);
+    }, []);
+  });
+};
 
-      callback();
-    });
+allocations.set = function (year, room, name) {
+  var update = {};
+  update['allocations.' + room] = name;
+  return db.allocations.update({year: +year}, {$set: update}, {upsert: true});
 };
 
 var navigation = exports.navigation = {};
-navigation.get = function (callback) {
-
+navigation.get = function () {
+  return db.navigation.find();
 };
