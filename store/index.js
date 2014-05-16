bba9b0e9125334b14510b77f7e44dda1bf1ea4f5
Forbes Lindesay <forbes@lindesay.co.uk>
2014-05-16 06:47:29 +0100

    Support offline development

--- store/index.js
+++ store/index.js
@@ -1,10 +1,13 @@
 'use strict';
 
 var assert = require('assert');
+var Promise = require('promise');
 var mongod = require('mongod');
 var db = mongod(process.env.ROOMS_MONGO, ['pages']);
+var writeFile = Promise.denodeify(require('fs').writeFile);
+var readFile = Promise.denodeify(require('fs').readFile);
 
-var pages = null;
+var pages = readFile(__dirname + '/pages.json', 'utf8').then(JSON.parse);
 
 exports.updatePageBody = function (id, body, isAdmin) {
   var query = isAdmin ? {_id: id} : {
@@ -33,8 +36,21 @@ exports.updatePageAllocation = function (id, year, name) {
 
 
 exports.getPages = function () {
-  return pages || (pages = db.pages.find().then());
+  var p = pages;
+  pages = db.pages.find().then(function (res) {
+    writeFile(__dirname + '/pages.json', JSON.stringify(res, null, '  '));
+    return res;
+  }, function (err) {
+    console.error(err.stack);
+    return p;
+  });
+  return p || pages;
 };
 exports.getPage = function (id) {
-  return db.pages.findOne({_id: id});
+  return exports.getPages().then(function (pages) {
+    pages = pages.filter(function (page) {
+      return page._id === id;
+    });
+    return pages.length === 1 ? pages[0] : null;
+  });
 };
